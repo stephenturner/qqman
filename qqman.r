874c428f5e6044583325e58ca672e5e94a100ec6
dcapurso <dancapurso1@gmail.com>
2013-04-11 21:31:21 -0700

    expanded features of manhattan()
    1. Optional color and size specification of points
    (pt.col=c('gray10','gray50'), pt.bg=c('gray10','gray50'), pt.cex=0.4)
    
    2. Optional gridlines
    (gridlines=T, gridlines.col='gray83')
    
    3. Option to make ymax soft (description below)
    (ymax=8, ymax.soft=T)
    # If ymax.soft==T, the specified value of ymax is the lower bound of ymax [i.e., if the data contains a larger value, then ymax=max(data)]. If ymax.soft==F, the specified value of ymax is absolute/hard.
    
    4. Optional color and lwd specification of significance lines
    (suggestiveline.col='blue',suggestiveline.lwd=1.5)
    
    5. Optional highlight color selection (including different colors for different regions)
    (highlight.col=c('green3','magenta'),highlight.bg=c('green3','magenta))
    
    6. Optional annotation of top snps
    (annotate=c('rs13895','rs20762'), annotate.font=3, annotate.cex=0.7)

--- qqman.r
+++ qqman.r
@@ -9,74 +9,199 @@
 # Altnernatively, use bmanhattan() , i.e., base manhattan. uses base graphics. way faster.
 
 
-## This is for testing purposes.
-# set.seed(42)
-# nchr=23
-# nsnps=1000
-# d=data.frame(
-#     SNP=sapply(1:(nchr*nsnps), function(x) paste("rs",x,sep='')),
-#     CHR=rep(1:nchr,each=nsnps), 
-#     BP=rep(1:nsnps,nchr), 
-#   P=runif(nchr*nsnps)
-# )
-# annotatesnps <- d$SNP[7550:7750]
-#pvector = d[,4] 
-#names(pvector) = d[,1] #passing pvector to qq() with names allows easy annotation (if indicated)
-#top_snps = c('rs20762','rs13895','rs11846')
-### Basic qq
-#qq(pvector,highlight=top_snps,annotate=top_snps)
+### This is for testing purposes. ######################################
+# 	set.seed(42)
+# 	nchr=23
+# 	nsnps=1000
+# 	d=data.frame(
+# 		SNP=sapply(1:(nchr*nsnps), function(x) paste("rs",x,sep='')),
+# 		CHR=rep(1:nchr,each=nsnps), 
+# 		BP=rep(1:nsnps,nchr), 
+# 		P=runif(nchr*nsnps)
+# 	)
+# 	### d[d$SNP=='rs20762',]$P = 1e-29
+# 	top_snps = c('rs13895','rs20762')
+# 	surrounding_snps = list(	as.character(d$SNP[13795:13995]),
+# 							as.character(d$SNP[20662:20862])) 
+
+### CALL:
+### manhattan(d,annotate=top_snps,highlight=surrounding_snps)
+### qq(d,annotate=top_snps,highlight=top_snps)
+#########################################################################
 
 # manhattan plot using base graphics
-manhattan <- function(dataframe, colors=c("gray10", "gray50"), ymax="max", limitchromosomes=1:23, suggestiveline=-log10(1e-5), genomewideline=-log10(5e-8), annotate=NULL, ...) {
-
-    d=dataframe
-    if (!("CHR" %in% names(d) & "BP" %in% names(d) & "P" %in% names(d))) stop("Make sure your data frame contains columns CHR, BP, and P")
+manhattan <- function(dataframe, chromosomes.limit=NULL,pt.col=c('gray10','gray50'),pt.bg=c('gray10','gray50'),
+	pt.cex=0.45,gridlines=F,gridlines.col='gray83',ymax=8, ymax.soft=T, annotate=NULL,annotate.cex=0.7,annotate.font=3,
+	suggestiveline=-log10(1e-5), suggestiveline.col='blue', genomewideline=-log10(5e-8), genomewideline.col='red',
+	suggestiveline.lwd=1.5,genomewideline.lwd=1.5, 
+	highlight=NULL,highlight.col=c('green3','magenta'),highlight.bg=c('green3','magenta'),  ...) {
+	#============================================================================================
+	######## Check data and arguments
+    d = na.omit(dataframe) # omit NAs
     
-    if (any(limitchromosomes)) d=d[d$CHR %in% limitchromosomes, ]
-    d=subset(na.omit(d[order(d$CHR, d$BP), ]), (P>0 & P<=1)) # remove na's, sort, and keep only 0<P<=1
+    if (!("CHR" %in% names(d) & "BP" %in% names(d) & "P" %in% names(d))) stop("Make sure your data frame contains columns CHR, BP, and P")
+    if (TRUE %in% is.na(suppressWarnings(as.numeric(d$CHR)))) warning('non-numeric, non-NA entries in CHR column of dataframe. attempting to remove..')
+    if (TRUE %in% is.na(suppressWarnings(as.numeric(d$BP)))) warning('non-numeric, non-NA entries in BP column of dataframe. attempting to remove..')
+	if (TRUE %in% is.na(suppressWarnings(as.numeric(d$P)))) warning('non-numeric, non-NA entries in P column of dataframe. attempting to remove..')
+    
+	d = d[!is.na(suppressWarnings(as.numeric(d$CHR))),] # remove rows with non-numeric, non-NA entries
+    d = d[!is.na(suppressWarnings(as.numeric(d$BP))),]
+    d = d[!is.na(suppressWarnings(as.numeric(d$P))),]
+    
+	
+	if (!is.null(annotate)){
+		if ('SNP' %in% names(d)){
+			if (FALSE %in% (annotate %in% d$SNP)) stop ("D'oh! Annotate vector must be a subset of the SNP column.")
+		} else {
+			stop("D'oh! Dataframe must have a column $SNP with rs_ids to use annotate feature.")
+		}
+	}
+	if (!is.numeric(annotate.cex) | annotate.cex<0) annotate.cex=0.7
+	if (!is.numeric(annotate.font)) annotate.font=3
+	
+	if (is.character(gridlines.col[1]) & !(gridlines.col[1] %in% colors())) gridlines.col = 'gray83'
+	if (!is.numeric(pt.cex) | pt.cex<0) pt.cex=0.45
+	if (is.character(pt.col) & (FALSE %in% (pt.col %in% colors()))) pt.col = c('gray10','gray50')
+	if (is.character(pt.bg) & (FALSE %in% (pt.bg %in% colors()))) pt.bg = F
+	if (is.character(highlight.col) & (FALSE %in% (highlight.col %in% colors()))) highlight.col = c('green3','magenta')
+	if (is.character(highlight.bg) & (FALSE %in% (highlight.bg %in% colors()))) highlight.bg = F
+	if (is.character(suggestiveline.col[1]) & !(suggestiveline.col[1] %in% colors())) suggestiveline.col = 'blue'
+	if (is.character(genomewideline.col[1]) & !(genomewideline.col[1] %in% colors())) genomewideline.col = 'red'
+		
+    if(!is.null(chromosomes.limit)){
+    	if (TRUE %in% is.na(suppressWarnings(as.numeric(chromosomes.limit)))){
+    		stop('chromosomes.limit argument is not numeric') 
+    	} else {  
+    		d = d[d$CHR %in% as.numeric(chromosomes.limit), ]
+    	}
+    }
+    
+
+    ######################
+    
+    # Set positions, ticks, and labels for plotting
+    d=subset(d[order(d$CHR, d$BP), ], (P>0 & P<=1)) # sort, and keep only 0<P<=1
     d$logp = -log10(d$P)
     d$pos=NA
-    ticks=NULL
-    lastbase=0
-    colors <- rep(colors,max(d$CHR))[1:max(d$CHR)]
-    if (ymax=="max") ymax<-ceiling(max(d$logp))
-    if (ymax<8) ymax<-8
     
-    numchroms=length(unique(d$CHR))
-    if (numchroms==1) {
+    # Ymax
+    if(is.na(suppressWarnings(as.numeric(ymax)))){  # not numeric
+    	ymax = ceiling(max(-log10(d$P)))
+    	warning('non-numeric ymax argument.')
+    } else if (as.numeric(ymax) < 0){ 			# negative
+    	ymax = ceiling(max(-log10(d$P)))
+    	warning('negative ymax argument.')
+    }
+    if (ymax.soft==T){ #if soft, ymax is just the lower limit for ymax
+    	ymax = max(ymax, ceiling(max(-log10(d$P))))
+    	
+    	# make ymax larger if top annotate SNP is very high
+    	if (!is.null(annotate)){
+    		annotate.max = max(d[which(d$SNP %in% annotate),]$logp)
+    		if ((ymax - annotate.max) < 0.18*ymax){
+    			ymax = annotate.max + 0.18*ymax
+    		}
+    	}
+    } #else, ymax = ymax
+	
+    nchr=length(unique(d$CHR))
+    if (nchr==1) {
         d$pos=d$BP
         ticks=floor(length(d$pos))/2+1
+        xlabel = paste('Chromosome',unique(d$CHR),'position')
+        labs = ticks
     } else {
+    	ticks = rep(NA,length(unique(d$CHR))+1)
+    	ticks[1] = 0
         for (i in unique(d$CHR)) {
-          if (i==1) {
-    			d[d$CHR==i, ]$pos=d[d$CHR==i, ]$BP
-    		} else {
-    			lastbase=lastbase+tail(subset(d,CHR==i-1)$BP, 1)
-    			d[d$CHR==i, ]$pos=d[d$CHR==i, ]$BP+lastbase
-    		}
-    		ticks=c(ticks, d[d$CHR==i, ]$pos[floor(length(d[d$CHR==i, ]$pos)/2)+1])
+          	d[d$CHR==i, ]$pos = d[d$CHR==i, ]$BP -d[d$CHR==i,]$BP[1] +1 +ticks[i]
+    		ticks[i+1] = max(d[d$CHR==i,]$pos)
+    	xlabel = 'Chromosome'
+    	labs = append(unique(d$CHR),'')
+    	}
+	}
+    
+    # Initialize plot
+    xmax = max(d$pos) * 1.03
+    xmin = max(d$pos) * -0.03
+    #ymax = ceiling(ymax * 1.03)
+    ymin = -ymax*0.03
+    plot(0,col=F,xaxt='n',bty='n',xaxs='i',yaxs='i',xlim=c(xmin,xmax), ylim=c(ymin,ymax),
+    		xlab=xlabel,ylab=expression(-log[10](italic(p))),las=1)
+	if (length(labs) > 10){
+		for (i in 1:length(labs)){
+			if (i %% 2 == 0){
+				labs[i] = ''
+			}
+		}
+	}
+	axis(1,at=ticks,labels=labs,lwd=0,lwd.ticks=1,cex.axis=0.95)
+	yvals = par('yaxp')
+	yinterval = par('yaxp')[2] / par('yaxp')[3]
+	axis(2,at= (seq(0,(ymax+yinterval/2),yinterval) - yinterval/2),labels=F,lwd=0,lwd.ticks=1,cex.axis=0.95)
+	
+    # Gridlines
+	if (isTRUE(gridlines)){
+		
+		abline(v=ticks,col=gridlines.col[1],lwd=1.0) #at ticks
+		abline(h=seq(0,ymax,yinterval),col=gridlines.col[1],lwd=1.0) # at labeled ticks
+		#abline(h=(seq(0,ymax,yinterval) - yinterval/2),col=gridlines.col[1],lwd=1.0) # at unlabeled ticks
+	}
+	
+    # Points, with optional highlighting
+    pt.col = rep(pt.col,max(d$CHR))[1:max(d$CHR)]
+	pt.bg = rep(pt.bg,max(d$CHR))[1:max(d$CHR)]
+    d.plain = d
+    if (!is.null(highlight)) {
+    	if(class(highlight)!='character' & class(highlight)!='list'){
+    		stop('"highlight" must be a char vector (for 1 color) or list (for multi color).')
+    	}
+    	
+    	if (class(highlight)=='character'){ #if char vector, make list for consistency in plotting below
+    		highlight = list(highlight)
+    	}
+    	
+    	if ('SNP' %in% names(d)){
+    		for (i in 1:length(highlight)){
+				if (FALSE %in% (highlight[[i]] %in% d$SNP)) stop ("D'oh! Highlight vector/list must be a subset of the SNP column.")
+			}
+		} else {
+			stop("D'oh! Dataframe must have a column $SNP with rs_ids to use highlight feature.")
+		}
+    	
+    	highlight.col = rep(highlight.col,length(highlight))[1:length(highlight)]
+		highlight.bg = rep(highlight.bg,length(highlight))[1:length(highlight)]
+    	
+    	for (i in 1:length(highlight)){
+    		d.plain = d.plain[which(!(d.plain$SNP %in% highlight[[i]])), ]
     	}
     }
     
-    if (numchroms==1) {
-        with(d, plot(pos, logp, ylim=c(0,ymax), ylab=expression(-log[10](italic(p))), xlab=paste("Chromosome",unique(d$CHR),"position"), ...))
-    }	else {
-        with(d, plot(pos, logp, ylim=c(0,ymax), ylab=expression(-log[10](italic(p))), xlab="Chromosome", xaxt="n", type="n", ...))
-        axis(1, at=ticks, lab=unique(d$CHR), ...)
-        icol=1
-        for (i in unique(d$CHR)) {
-            with(d[d$CHR==i, ],points(pos, logp, col=colors[icol], ...))
-            icol=icol+1
+    icol=1
+    for (i in unique(d.plain$CHR)) {
+        with(d.plain[d.plain$CHR==i, ],points(pos, logp, col=pt.col[icol],bg=pt.bg[icol],pch=21,cex=pt.cex,...))
+        icol=icol+1
+    }
+    
+    if (!is.null(highlight)){	
+    	for (i in 1:length(highlight)){
+    		d.highlight=d[which(d$SNP %in% highlight[[i]]), ]
+    		with(d.highlight, points(pos, logp, col=highlight.col[i],bg=highlight.bg[i], pch=21,cex=pt.cex, ...)) 
     	}
     }
     
-    if (!is.null(annotate)) {
-        d.annotate=d[which(d$SNP %in% annotate), ]
-        with(d.annotate, points(pos, logp, col="green3", ...)) 
-    }
-    
-    if (suggestiveline) abline(h=suggestiveline, col="blue")
-    if (genomewideline) abline(h=genomewideline, col="red")
+    # Significance lines
+    if (is.numeric(suggestiveline)) abline(h=suggestiveline, col=suggestiveline.col[1],lwd=1.5)
+    if (is.numeric(genomewideline)) abline(h=genomewideline, col=genomewideline.col[1],lwd=1.5)
+
+	# Annotate
+	if (!is.null(annotate)){
+		d.annotate = d[which(d$SNP %in% annotate),]
+		text(d.annotate$pos,(d.annotate$logp + 0.019*ymax),labels=d.annotate$SNP,srt=90,cex=annotate.cex,adj=c(0,0.48),font=annotate.font)		
+	}
+
+	# Box
+	box()
 }
 
 
