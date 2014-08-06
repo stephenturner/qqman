28dbc4390e6ed6ea2c52cb78bc485ce70986019f
Stephen Turner <vustephen@gmail.com>
2014-08-06 17:17:57 -0400

    updated qq to allow setting graphical parameters using the same plan as with manhattan()

--- R/qq.R
+++ R/qq.R
@@ -26,12 +26,27 @@ qq = function(pvector, ...) {
     o = -log10(sort(pvector,decreasing=FALSE))
     e = -log10( ppoints(length(pvector) ))
     
-    # Create the plot
-    plot(e, o, pch=20, 
-         xlab=expression(Expected~~-log[10](italic(p))), 
-         ylab=expression(Observed~~-log[10](italic(p))), 
-         ...)
     
+#     # The old way
+#     plot(e, o, pch=20, 
+#          xlab=expression(Expected~~-log[10](italic(p))), 
+#          ylab=expression(Observed~~-log[10](italic(p))), 
+#          ...)
+    
+    # The new way to initialize the plot.
+    ## See http://stackoverflow.com/q/23922130/654296
+    ## First, define your default arguments
+    def_args <- list(pch=20, xlim=c(0, max(e)), ylim=c(0, max(o)), 
+                     xlab=expression(Expected~~-log[10](italic(p))), 
+                     ylab=expression(Observed~~-log[10](italic(p)))
+    )
+    ## Next, get a list of ... arguments
+    #dotargs <- as.list(match.call())[-1L]
+    dotargs <- list(...)
+    ## And call the plot function passing NA, your ... arguments, and the default
+    ## arguments that were not defined in the ... arguments.
+    tryCatch(do.call("plot", c(list(x=e, y=o), def_args[!names(def_args) %in% names(dotargs)], dotargs)), warn=stop)
+
     # Add diagonal
     abline(0,1,col="red")
     
