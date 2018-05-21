ee2620ee0d195e820b6741a675db1c97ad8528e7
Stephen Turner <vustephen@gmail.com>
2018-05-21 08:57:07 -0400

    update citations

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,6 @@
     packageStartupMessage("For example usage please run: vignette('qqman')")
     packageStartupMessage()
     packageStartupMessage("Citation appreciated but not required:")
-    packageStartupMessage("Turner, S.D. qqman: an R package for visualizing GWAS results using Q-Q and manhattan plots. biorXiv DOI: 10.1101/005165 (2014).")
+    packageStartupMessage("Turner, (2018). qqman: an R package for visualizing GWAS results using Q-Q and manhattan plots. Journal of Open Source Software, 3(25), 731, https://doi.org/10.21105/joss.00731.")
     packageStartupMessage()
 }
